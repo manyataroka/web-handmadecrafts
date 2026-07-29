6be17329149c4a04cf3a5253c4dac2c9f57f6123
Manyata Roka <161047309+JuniorManyata@users.noreply.github.com>
2026-07-29 08:19:40 +0545

    Update

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,9 +1,9 @@
-// export default function AdminLayout({ children }: { children: React.ReactNode }) {
-//     return (
-//         <section>
-//             <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-8">
-//                 {children}
-//             </main>
-//         </section>
-//     );
-// }
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <section>
+            <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-8">
+                {children}
+            </main>
+        </section>
+    );
+}
